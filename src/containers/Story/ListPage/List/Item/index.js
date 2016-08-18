1ffd102521f714796d1906954035aa262d4edaad
Wei Zhu <yesmeck@gmail.com>
2016-08-18 21:58:44 +0800

    more details

--- src/containers/Story/ListPage/List/Item/index.js
+++ src/containers/Story/ListPage/List/Item/index.js
@@ -1,25 +1,53 @@
 import React from 'react'
+import { Link } from 'feeble/router'
+import { host, timeAgo } from '../../../../../helpers'
 import './style.css'
 
 export default function Item({ story }) {
+  let title= null
+  if (story.url) {
+    title = (
+      <span className="title">
+        <a href={story.url} target="_blank">{story.title}</a>
+        <span className="host">({host(story.url)})</span>
+      </span>
+    )
+  } else {
+    title = (
+      <span className="title">
+        <Link to={`/item/${story.id}`}>{story.title}</Link>
+      </span>
+    )
+  }
+
+  let commentLink = null
+  if (story.type !== 'job') {
+    commentLink = (
+      <span className="comments-link">
+        | <Link to={`/item/${story.id}`}>{story.descendants} comments</Link>
+      </span>
+    )
+  }
+
+  let label = null
+  if (story.type !== 'story') {
+    label = <span className="label">{story.type}</span>
+  }
+
   return (
     <li className="news-item">
       <span className="score">{story.score}</span>
-      <span className="title">
-        <a href={story.url} target="_blank">{story.title}</a>
-        <span className="host">(iamtrask.github.io)</span>
-      </span>
+      {title}
       <br />
       <span className="meta">
         <span className="by">
-          by <a href="/user/williamtrask" className="">{story.by}</a>
+          by <Link to={`/user/${story.by}`} className="">{story.by}</Link>
         </span>
         <span className="time">
-          6 hours ago
+          {timeAgo(story.time)} ago
         </span>
-        <span className="comments-link">
-          | <a href="/item/12309777" className="">{story.descendants} comments</a>
-        </span>
+        {commentLink}
+        {label}
       </span>
     </li>
   )
